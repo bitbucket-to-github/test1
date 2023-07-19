ed241c65eadfc506690a136eb657df096c361731
M M <madhavf777@gmail.com>
2023-07-19 07:09:22 +0000

    groovy script changed

--- Jenkins-pipeline.groovy
+++ Jenkins-pipeline.groovy
@@ -1,53 +1,52 @@
-pipeline { 
+pipeline {
 
-        agent any 
+    agent any
 
-        stages { 
+    stages {
 
-          stage('pull-code') { 
+        stage('pull-code') {
 
-            steps { 
+            steps {
 
-                  git credentialsId: 'jenkins', url: 'https://madhav_mahamuni@bitbucket.org/fs-bitbucket/fs_employee.git' 
+                git credentialsId: 'jenkins', url: 'https://madhav_mahamuni@bitbucket.org/fs-bitbucket/fs_employee.git'
 
-            } 
-            
-          } 
-
-          stage('Build') { 
-
-            steps { 
-
-                sh 'mvn clean package' 
-
-            } 
-
-          } 
-          
-          stage('Test') {
-              
-            steps('SonarQube Analysis') {
-                
-                def mvn = tool 'Default Maven';
-                 
-                withSonarQubeEnv() {
-                      
-                sh "${mvn}/bin/mvn clean verify sonar:sonar -Dsonar.projectKey=fs-bitbucket_fs_employee_AYlPLJp_qqhibVYfSfU_"
-                  
             }
-                
-          }
 
-          stage('Deploy') { 
+        }
 
-            steps { 
+        stage('Build') {
 
-               deploy adapters: [tomcat9(credentialsId: 'tomcat', path: '/home/fs-shubhranshu/Tomcat/webapps', url: 'http://localhost:8081')], contextPath: '/', war: '**/target/*.war' 
+            steps {
 
-            } 
+                sh 'mvn clean package'
 
-          } 
+            }
 
-        } 
-        
-    } 
+        }
+
+        stage('Test') {
+
+            steps('SonarQube Analysis') {
+
+                def mvn = tool 'Default Maven';
+
+                withSonarQubeEnv() {
+
+                    sh "${mvn}/bin/mvn clean verify sonar:sonar -Dsonar.projectKey=fs-bitbucket_fs_employee_AYlPLJp_qqhibVYfSfU_"
+
+                }
+
+            }
+
+            stage('Deploy') {
+
+                steps {
+
+                    deploy adapters: [tomcat9(credentialsId: 'tomcat', path: '/home/fs-shubhranshu/Tomcat/webapps', url: 'http://localhost:8081')], contextPath: '/', war: '**/target/*.war'
+
+                }
+
+            }
+
+        }
+    }
