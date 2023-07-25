fa357534e5ede98d30318f8df044360be58d220a
M M <madhavf777@gmail.com>
2023-07-25 07:15:53 +0000

    Jenkins-pipeline.groovy edited online with Bitbucket

--- Jenkins-pipeline.groovy
+++ Jenkins-pipeline.groovy
@@ -40,8 +40,8 @@ pipeline {
         stage('Deploy') {
 
             steps {
-
-                    deploy adapters: [tomcat9(credentialsId: 'tomcat', path: '/home/fs-shubhranshu/Tomcat/webapps', url: 'http://localhost:8081')], contextPath: '/', war: '/var/lib/jenkins/workspace/ci-cd pipeline/target/*.war'
+                    sudo cp -r /var/lib/jenkins/workspace/ci-cd pipeline/target/studentapp-2.2-SNAPSHOT.war /home/fs-shubhranshu/Tomcat/apache-tomcat-9.0.78/webapps
+                    //deploy adapters: [tomcat9(credentialsId: 'tomcat', path: '/home/fs-shubhranshu/Tomcat/webapps', url: 'http://localhost:8081')], contextPath: '/', war: '/var/lib/jenkins/workspace/ci-cd pipeline/target/*.war'
 
             }
 
